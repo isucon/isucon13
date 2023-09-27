62c041af22e2b7d7e4018fd715857c42b3ae5acf
tukeJonny <ne250143@yahoo.co.jp>
2023-09-27 11:32:22 +0900

    wip

--- bench/internal/scheduler/reservation_scheduler.go
+++ bench/internal/scheduler/reservation_scheduler.go
@@ -1,17 +1,33 @@
 package scheduler
 
-import "sync"
+import (
+	"sync"
+	"time"
+)
 
-type ReserveTimeSlice struct {
+var reservationScheduler = newReservationScheduler()
+
+type ReservationTimeSlice struct {
+	Id      int
+	UserId  int
+	StartAt time.Time
+	EndAt   time.Time
 }
 
 type ReservationScheduler struct {
-	mu sync.RWMutex
+	mu                    sync.RWMutex
+	reservationTimeSlices []*ReservationTimeSlice
 }
 
 func newReservationScheduler() *ReservationScheduler {
-	// FIXME: impl
 	return nil
 }
 
-var reservationScheduler = newReservationScheduler()
+// FIXME: シード生成したシーズン毎のスケジュールをもとにしてデータ生成を行う
+
+func (r *ReservationScheduler) AddReservation(timeSlice *ReservationTimeSlice) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	r.reservationTimeSlices = append(r.reservationTimeSlices, timeSlice)
+}
---
+++ bench/internal/scheduler/vtuber_scheduler.go
@@ -0,0 +1,16 @@
+package scheduler
+
+// 新人VTuber ... 当該シーズンにおいて登録した配信者
+// 人気VTuber ... 古株のうち、人気の高いVTuber
+// 不人気VTuber ... 古株のうち、人気が高くないVTuber
+
+type VTuberScheduler struct {
+}
+
+func (s *VTuberScheduler) SelectPopularVTuber() {
+
+}
+
+func (s *VTuberScheduler) SelectUnpopularVTuber() {
+
+}
