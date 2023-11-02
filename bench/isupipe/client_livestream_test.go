ae9f1c115716befd8f510fbe3777fd95bf6b3fe5
tukeJonny <ne250143@yahoo.co.jp>
2023-11-02 19:46:26 +0900

    add test

--- bench/isupipe/client_livestream_test.go
+++ bench/isupipe/client_livestream_test.go
@@ -1,3 +1,33 @@
 package isupipe
 
+import (
+	"context"
+	"testing"
+
+	"github.com/isucon/isucandar/agent"
+	"github.com/isucon/isucon13/bench/internal/config"
+	"github.com/isucon/isucon13/bench/internal/scheduler"
+	"github.com/stretchr/testify/assert"
+)
+
 // FIXME: 予約期間、枠数などテスト
+
+func TestLivestreamSearch(t *testing.T) {
+	ctx := context.Background()
+
+	client, err := NewClient(
+		agent.WithBaseURL(config.TargetBaseURL),
+	)
+	assert.NoError(t, err)
+
+	user := scheduler.UserScheduler.GetRandomStreamer()
+	err = client.Login(ctx, &LoginRequest{
+		UserName: user.Name,
+		Password: user.RawPassword,
+	})
+	assert.NoError(t, err)
+
+	livestreams, err := client.SearchLivestreams(ctx)
+	assert.NoError(t, err)
+	assert.NotZero(t, len(livestreams))
+}
