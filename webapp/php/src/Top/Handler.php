e1eb895721e3e1dcd04d66abfb04830fd8d16fa4
Okada Shohei <okashoicircus0@gmail.com>
2023-11-03 13:54:08 +0900

    [php] implement GET /api/tag

--- webapp/php/src/Top/Handler.php
+++ webapp/php/src/Top/Handler.php
@@ -5,15 +5,52 @@ declare(strict_types=1);
 namespace IsuPipe\Top;
 
 use IsuPipe\AbstractHandler;
+use PDO;
+use PDOException;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Exception\HttpInternalServerErrorException;
 
 class Handler extends AbstractHandler
 {
+    public function __construct(
+        private PDO $db,
+    ) {
+    }
+
     public function getTagHandler(Request $request, Response $response): Response
     {
-        // TODO: 実装
-        return $response;
+        $this->db->beginTransaction();
+
+        /** @var list<TagModel> $tagModels */
+        $tagModels = [];
+        try {
+            $stmt = $this->db->query('SELECT * FROM tags');
+            while (($row = $stmt->fetch()) !== false) {
+                $tagModels[] = TagModel::fromRow($row);
+            }
+        } catch (PDOException $e) {
+            throw new HttpInternalServerErrorException(
+                request: $request,
+                message: 'failed to get tags',
+                previous: $e,
+            );
+        }
+
+        $this->db->commit();
+
+        /** @var list<Tag> $tags */
+        $tags = [];
+        foreach ($tagModels as $tagModel) {
+            $tags[] = new Tag(
+                id: $tagModel->id,
+                name: $tagModel->name,
+            );
+        }
+
+        return $this->jsonResponse($response, new TagsResponse(
+            tags: $tags
+        ));
     }
 
     /**
