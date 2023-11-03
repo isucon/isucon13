083390817315aa2555aa75d3e54d00aff2d61f8d
Okada Shohei <okashoicircus0@gmail.com>
2023-11-03 13:50:42 +0900

    [php] implement POST /api/initialize

--- webapp/php/src/InitializeHandler.php
+++ webapp/php/src/InitializeHandler.php
@@ -4,15 +4,36 @@ namespace IsuPipe;
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Log\LoggerInterface as Logger;
+use RuntimeException;
+use Slim\Exception\HttpInternalServerErrorException;
 
 /**
  * FIXME: ポータルと足並み揃えて修正
  */
 class InitializeHandler extends AbstractHandler
 {
+    public function __construct(
+        private Logger $logger,
+    ) {
+    }
+
     public function __invoke(Request $request, Response $response): Response
     {
-        // TODO: 実装
-        return $response;
+        try {
+            $this->execCommand([__DIR__ . '/../../sql/init.sh']);
+        } catch (RuntimeException $e) {
+            $this->logger->warning('init.sh failed with err=' . $e->getMessage());
+            throw new HttpInternalServerErrorException(
+                request: $request,
+                message: 'failed to initialize',
+                previous: $e,
+            );
+        }
+
+        return $this->jsonResponse($response, new InitializeResponse(
+            advertiseLevel: 10,
+            language: 'php',
+        ));
     }
 }
