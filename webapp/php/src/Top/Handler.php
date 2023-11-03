1a2c7741897b9e7ab90378eafc8a50a0e82ba9e4
Okada Shohei <okashoicircus0@gmail.com>
2023-11-03 14:26:09 +0900

    [php] implement GET /api/user/:username/theme

--- webapp/php/src/Top/Handler.php
+++ webapp/php/src/Top/Handler.php
@@ -5,16 +5,21 @@ declare(strict_types=1);
 namespace IsuPipe\Top;
 
 use IsuPipe\AbstractHandler;
+use IsuPipe\User\{ Theme, ThemeModel, UserModel, VerifyUserSession };
 use PDO;
 use PDOException;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Exception\HttpInternalServerErrorException;
+use Slim\Exception\{ HttpInternalServerErrorException, HttpNotFoundException };
+use SlimSession\Helper as Session;
 
 class Handler extends AbstractHandler
 {
+    use VerifyUserSession;
+
     public function __construct(
         private PDO $db,
+        private Session $session,
     ) {
     }
 
@@ -56,10 +61,64 @@ class Handler extends AbstractHandler
     /**
      * 配信者のテーマ取得API
      * GET /api/user/:username/theme
+     *
+     * @param array<string, string> $params
      */
-    public function getStreamerThemeHandler(Request $request, Response $response): Response
+    public function getStreamerThemeHandler(Request $request, Response $response, array $params): Response
     {
-        // TODO: 実装
-        return $response;
+        $this->verifyUserSession($request, $this->session);
+
+        $username = $params['username'] ?? '';
+
+        $this->db->beginTransaction();
+
+        try {
+            $stmt = $this->db->prepare('SELECT id FROM users WHERE name = ?');
+            $stmt->bindValue(1, $username);
+            $stmt->execute();
+            $row = $stmt->fetch();
+        } catch (PDOException $e) {
+            throw new HttpInternalServerErrorException(
+                request: $request,
+                message: 'failed to get user',
+                previous: $e,
+            );
+        }
+        if ($row === false) {
+            throw new HttpNotFoundException(
+                request: $request,
+                message: 'not found user that has the given username',
+            );
+        }
+        $userModel = UserModel::fromRow($row);
+
+        try {
+            $stmt = $this->db->prepare('SELECT * FROM themes WHERE user_id = ?');
+            $stmt->bindValue(1, $userModel->id, PDO::PARAM_INT);
+            $stmt->execute();
+            $row = $stmt->fetch();
+        } catch (PDOException $e) {
+            throw new HttpInternalServerErrorException(
+                request: $request,
+                message: 'failed to get user theme',
+                previous: $e,
+            );
+        }
+        if ($row === false) {
+            throw new HttpInternalServerErrorException(
+                request: $request,
+                message: 'failed to get user theme',
+            );
+        }
+        $themeModel = ThemeModel::fromRow($row);
+
+        $this->db->commit();
+
+        $theme = new Theme(
+            id: $themeModel->id,
+            darkMode: $themeModel->darkMode,
+        );
+
+        return $this->jsonResponse($response, $theme);
     }
 }
