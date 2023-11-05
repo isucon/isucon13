54e2786e401696e01372e7288bd6b2e8f4131c0f
tukeJonny <ne250143@yahoo.co.jp>
2023-11-05 15:53:11 +0900

    ngwordsの初期データ追加

--- webapp/init.sh
+++ webapp/init.sh
@@ -53,8 +53,17 @@ mysql -u"$ISUCON_DB_USER" \
 		--port "$ISUCON_DB_PORT" \
 		"$ISUCON_DB_NAME" < initial_livecomments.sql
 
+<<<<<<< HEAD
 pdnsutil delete-zone u.isucon.dev
 pdnsutil create-zone u.isucon.dev
 pdnsutil add-record u.isucon.dev . A 30 $ISUCON_SUBDOMAIN_ADDRESS
 pdnsutil add-record u.isucon.dev pipe A 30 $ISUCON_SUBDOMAIN_ADDRESS
 pdnsutil add-record u.isucon.dev test001 A 30 $ISUCON_SUBDOMAIN_ADDRESS
+=======
+mysql -u"$ISUCON_DB_USER" \
+		-p"$ISUCON_DB_PASSWORD" \
+		--host "$ISUCON_DB_HOST" \
+		--port "$ISUCON_DB_PORT" \
+		"$ISUCON_DB_NAME" < initial_ngwords.sql
+
+>>>>>>> bc1e3c6 (ngwordsの初期データ追加)
