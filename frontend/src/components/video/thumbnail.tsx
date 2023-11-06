1ae0f9f44118f63fba00b349e66212c02dc0747c
kamijin_fanta <kamijin@live.jp>
2023-11-06 17:46:10 +0900

    frontend: view statistics in thumbnail

--- frontend/src/components/video/thumbnail.tsx
+++ frontend/src/components/video/thumbnail.tsx
@@ -6,6 +6,7 @@ import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 import { ja } from 'date-fns/locale';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useLiveStreamStatistics, useMedia } from '~/api/hooks';
 import { Schemas } from '~/api/types';
 
 export interface VideoThumbnailProps {
@@ -16,22 +17,21 @@ export function VideoThumbnail({
 }: VideoThumbnailProps): React.ReactElement {
   const date = React.useMemo(
     () =>
-      liveSteram.created_at
-        ? formatDistanceToNow(liveSteram.created_at * 1000, {
+      liveSteram.start_at
+        ? formatDistanceToNow(liveSteram.start_at * 1000, {
             addSuffix: true,
             locale: ja,
           })
         : 'unkown',
-    [liveSteram.created_at],
+    [liveSteram.start_at],
   );
+  const media = useMedia(liveSteram.id ?? '');
+  const statistics = useLiveStreamStatistics(liveSteram.id?.toString() ?? null);
 
   return (
     <Link to={`/watch/${liveSteram.id}`} style={{ textDecoration: 'none' }}>
       <AspectRatio sx={{ borderRadius: 10 }}>
-        <img
-          src="https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=400"
-          loading="lazy"
-        />
+        <img src={media.data?.thumbnail_url} loading="lazy" />
       </AspectRatio>
       <Stack direction="row" spacing={1} sx={{ marginTop: 1 }}>
         <Avatar />
@@ -41,7 +41,7 @@ export function VideoThumbnail({
             <Stack direction="row" spacing={2}>
               <span>{liveSteram.owner?.name}</span>
               <span>
-                {liveSteram.viewers_count}人視聴・{date}
+                {statistics.data?.viewers_count}人視聴・{date}
               </span>
             </Stack>
           </Typography>
