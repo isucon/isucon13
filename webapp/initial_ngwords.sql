39c88933875c1f602a058bd141cf713e5aaeee5e
tukeJonny <ne250143@yahoo.co.jp>
2023-11-07 03:23:30 +0900

    Revert "ngword削減"
    This reverts commit b6a04247b9b37e00d971392b24fbba4060ea31ad.

--- webapp/initial_ngwords.sql
+++ webapp/initial_ngwords.sql
@@ -1,4 +1,11 @@
 INSERT INTO ng_words (user_id, livestream_id, word, created_at)
 VALUES
+	(1, 1, '2相コミット', UNIX_TIMESTAMP()),
+	(1, 1, '3DES', UNIX_TIMESTAMP()),
 	(1, 1, 'Aレコード', UNIX_TIMESTAMP()),
+	(1, 1, '3相コミット', UNIX_TIMESTAMP()),
+	(1, 1, 'AAAAレコード', UNIX_TIMESTAMP()),
+	(1, 1, 'BGP', UNIX_TIMESTAMP()),
+	(1, 1, 'Anycast', UNIX_TIMESTAMP()),
+	(1, 1, 'ACIDプロパティ', UNIX_TIMESTAMP()),
 	(1, 1, '椅子パイプ', UNIX_TIMESTAMP());
