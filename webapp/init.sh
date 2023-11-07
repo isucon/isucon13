502563b0df521d78732ecfa25dafa4648cb7c116
tukeJonny <ne250143@yahoo.co.jp>
2023-11-08 01:30:17 +0900

    fix

--- webapp/init.sh
+++ webapp/init.sh
@@ -69,7 +69,6 @@ pdnsutil delete-zone u.isucon.dev
 pdnsutil create-zone u.isucon.dev
 pdnsutil add-record u.isucon.dev . NS 3600 ns1.u.isucon.dev
 pdnsutil add-record u.isucon.dev . A 30 $ISUCON_SUBDOMAIN_ADDRESS
-pdnsutil add-record u.isucon.dev '*' A 30 $ISUCON_SUBDOMAIN_ADDRESS
-#pdnsutil add-record u.isucon.dev pipe A 30 $ISUCON_SUBDOMAIN_ADDRESS
-#pdnsutil add-record u.isucon.dev test001 A 30 $ISUCON_SUBDOMAIN_ADDRESS
+pdnsutil add-record u.isucon.dev pipe A 30 $ISUCON_SUBDOMAIN_ADDRESS
+pdnsutil add-record u.isucon.dev test001 A 30 $ISUCON_SUBDOMAIN_ADDRESS
 
