cc6ef5857f57d2a6834a222e24a2225024e039b9
YutaUra <yuuta3594@gmail.com>
2023-11-10 00:10:30 +0900

    [node.js] use formatter

--- webapp/node/src/node.ts
+++ webapp/node/src/node.ts
@@ -1,14 +1,14 @@
-import { serve } from "@hono/node-server";
-import { createApp, Deps } from "./create-app";
-import { exec } from "node:child_process";
-import { promisify } from "node:util";
+import { serve } from '@hono/node-server'
+import { createApp, Deps } from './create-app'
+import { exec } from 'node:child_process'
+import { promisify } from 'node:util'
 
-const execAsync = promisify(exec);
+const execAsync = promisify(exec)
 
 const deps = {
   exec: async (cmd: string) => execAsync(cmd),
-} satisfies Deps;
+} satisfies Deps
 
 serve({ ...createApp(deps), port: 8080 }, (add) =>
-  console.log(`Listening on http://localhost:${add.port}`)
-);
+  console.log(`Listening on http://localhost:${add.port}`),
+)
