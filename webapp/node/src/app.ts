bd5c3dae482526c88ea773ec445f563c80ed91a3
YutaUra <yuuta3594@gmail.com>
2023-11-09 23:28:11 +0900

    node.js でベンチマークができる状態にする

--- webapp/node/src/app.ts
+++ webapp/node/src/app.ts
@@ -2,4 +2,6 @@ import { Hono } from "hono";
 
 export const app = new Hono();
 
-app.get("/", (c) => c.text("Hono!"));
+app.post("/api/initialize", (c) =>
+  c.json({ advertise_level: 10, advertise_name: "node" })
+);
--- webapp/node/src/node.ts
+++ webapp/node/src/node.ts
@@ -1,4 +1,6 @@
 import { serve } from "@hono/node-server";
 import { app } from "./app";
 
-serve(app, (add) => console.log(`Listening on http://localhost:${add.port}`));
+serve({ ...app, port: 8080 }, (add) =>
+  console.log(`Listening on http://localhost:${add.port}`)
+);
