ff291c16f13c6b3f19888416364367c0713ce1b7
YutaUra <yuuta3594@gmail.com>
2023-11-12 03:50:29 +0900

    [node.js] implement POST /api/livestream/:livestream_id/reaction

--- webapp/node/src/handlers/reaction-handler.ts
+++ webapp/node/src/handlers/reaction-handler.ts
@@ -1,9 +1,12 @@
 import { Hono } from 'hono'
-import { ResultSetHeader } from 'mysql2/promise'
+import { ResultSetHeader, RowDataPacket } from 'mysql2/promise'
 import { ApplicationDeps, HonoEnvironment } from '../types'
 import { verifyUserSessionMiddleware } from '../middlewares/verify-user-session-middleare'
 import { defaultUserIDKey } from '../contants'
-import { makeReactionResponse } from '../utils/make-reaction-response'
+import {
+  ReactionResponse,
+  makeReactionResponse,
+} from '../utils/make-reaction-response'
 
 export const reactionHandler = (deps: ApplicationDeps) => {
   const handler = new Hono<HonoEnvironment>()
@@ -55,6 +58,64 @@ export const reactionHandler = (deps: ApplicationDeps) => {
       }
 
       return c.json(reactionResponse, 201)
+    },
+  )
+
+  handler.get(
+    '/api/livestream/:livestream_id/reaction',
+    verifyUserSessionMiddleware,
+    async (c) => {
+      const livestreamId = Number.parseInt(c.req.param('livestream_id'), 10)
+      if (Number.isNaN(livestreamId)) {
+        return c.text('livestream_id in path must be integer', 400)
+      }
+
+      await deps.connection.beginTransaction()
+
+      let query =
+        'SELECT * FROM reactions WHERE livestream_id = ? ORDER BY created_at DESC'
+      const limit = c.req.query('limit')
+      if (limit) {
+        const limitNumber = Number.parseInt(limit, 10)
+        if (Number.isNaN(limitNumber)) {
+          return c.text('limit query parameter must be integer', 400)
+        }
+        query += ` LIMIT ${limitNumber}`
+      }
+
+      const reactions = await deps.connection
+        .query<RowDataPacket[]>(query, [livestreamId])
+        .then(([results]) => ({ ok: true, data: results }) as const)
+        .catch((error) => ({ ok: false, error }) as const)
+      if (!reactions.ok) {
+        await deps.connection.rollback()
+        return c.text('failed to get reactions', 500)
+      }
+
+      const reactionResponses: ReactionResponse[] = []
+      for (const reaction of reactions.data) {
+        const reactionResponse = await makeReactionResponse(deps, {
+          id: reaction.id,
+          emoji_name: reaction.emoji_name,
+          user_id: reaction.user_id,
+          livestream_id: reaction.livestream_id,
+          created_at: reaction.created_at,
+        })
+        if (!reactionResponse.ok) {
+          await deps.connection.rollback()
+          return c.text(reactionResponse.error, 500)
+        }
+        reactionResponses.push(reactionResponse.data)
+      }
+
+      try {
+        await deps.connection.commit()
+      } catch {
+        await deps.connection.rollback()
+        return c.text('failed to commit', 500)
+      }
+
+      return c.json(reactionResponses)
     },
   )
 
