a7f95fe388ebffbe5031b74520f7caf306114d1c
Okada Shohei <okashoicircus0@gmail.com>
2023-11-17 16:11:01 +0900

    [php] implement FillLivecommentReportResponse

---
+++ webapp/php/src/Livecomment/FillLivecommentReportResponse.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace IsuPipe\Livecomment;
+
+use IsuPipe\User\{ FillUserResponse, UserModel };
+use PDO;
+use RuntimeException;
+
+trait FillLivecommentReportResponse
+{
+    use FillLivecommentResponse, FillUserResponse;
+
+    /**
+     * @throws RuntimeException
+     */
+    protected function fillLivecommentReportResponse(LivecommentReportModel $reportModel, PDO $db): LivecommentReport
+    {
+        $stmt = $db->prepare('SELECT * FROM users WHERE id = ?');
+        $stmt->bindValue(1, $reportModel->userId, PDO::PARAM_INT);
+        $stmt->execute();
+        $row = $stmt->fetch();
+        if ($row === false) {
+            throw new RuntimeException();
+        }
+        $reporterModel = UserModel::fromRow($row);
+        $reporter = $this->fillUserResponse($reporterModel, $db);
+
+        $stmt = $db->prepare('SELECT * FROM livecomments WHERE id = ?');
+        $stmt->bindValue(1, $reportModel->livecommentId, PDO::PARAM_INT);
+        $stmt->execute();
+        $row = $stmt->fetch();
+        if ($row === false) {
+            throw new RuntimeException();
+        }
+        $livecommentModel = LivecommentModel::fromRow($row);
+        $livecomment = $this->fillLivecommentResponse($livecommentModel, $db);
+
+        return new LivecommentReport(
+            id: $reportModel->id,
+            reporter: $reporter,
+            livecomment: $livecomment,
+            createdAt: $reportModel->createdAt,
+        );
+    }
+}
