4c149e7249b617279fbe973a7d00351339edf42b
Okada Shohei <okashoicircus0@gmail.com>
2023-11-17 18:12:32 +0900

    [php] implement GET /api/livestream/:livestream_id/reaction

--- webapp/php/src/Reaction/Handler.php
+++ webapp/php/src/Reaction/Handler.php
@@ -6,20 +6,96 @@ namespace IsuPipe\Reaction;
 
 use IsuPipe\AbstractHandler;
 use IsuPipe\Livestream\{ FillLivestreamResponse, LivestreamModel };
-use IsuPipe\User\{ FillUserResponse, UserModel };
+use IsuPipe\User\{ FillUserResponse, UserModel, VerifyUserSession };
 use PDO;
+use PDOException;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use RuntimeException;
+use Slim\Exception\{ HttpBadRequestException, HttpInternalServerErrorException, HttpNotFoundException };
+use SlimSession\Helper as Session;
 
 class Handler extends AbstractHandler
 {
-    use FillLivestreamResponse, FillUserResponse;
+    use FillLivestreamResponse, FillUserResponse, VerifyUserSession;
 
-    public function getReactionsHandler(Request $request, Response $response): Response
+    public function __construct(
+        private PDO $db,
+        private Session $session,
+    ) {
+    }
+
+    /**
+     * @param array<string, string> $params
+     */
+    public function getReactionsHandler(Request $request, Response $response, array $params): Response
     {
-        // TODO: 実装
-        return $response;
+        $this->verifyUserSession($request, $this->session);
+
+        $livestreamIdStr = $params['livestream_id'] ?? '';
+        if ($livestreamIdStr === '') {
+            throw new HttpBadRequestException(
+                request: $request,
+                message: 'livestream_id in path must be integer',
+            );
+        }
+        $livestreamId = filter_var($livestreamIdStr, FILTER_VALIDATE_INT);
+        if (!is_int($livestreamId)) {
+            throw new HttpBadRequestException(
+                request: $request,
+                message: 'livestream_id in path must be integer',
+            );
+        }
+
+        $this->db->beginTransaction();
+
+        $query = 'SELECT * FROM reactions WHERE livestream_id = ? ORDER BY created_at DESC';
+        $limitStr = $request->getQueryParams()['limit'] ?? '';
+        if ($limitStr !== '') {
+            $limit = filter_var($limitStr, FILTER_VALIDATE_INT);
+            if (!is_int($limit)) {
+                throw new HttpBadRequestException(
+                    request: $request,
+                    message: 'limit query parameter must be integer',
+                );
+            }
+            $query .= sprintf(' LIMIT %d', $limit);
+        }
+
+        /** @var list<ReactionModel> $reactionModels */
+        $reactionModels = [];
+        try {
+            $stmt = $this->db->prepare($query);
+            $stmt->bindValue(1, $livestreamId, PDO::PARAM_INT);
+            $stmt->execute();
+            while (($row = $stmt->fetch()) !== false) {
+                $reactionModels[] = ReactionModel::fromRow($row);
+            }
+        } catch (PDOException $e) {
+            throw new HttpInternalServerErrorException(
+                request: $request,
+                message: 'failed to get reactions',
+                previous: $e,
+            );
+        }
+
+        /** @var list<Reaction> $reactions */
+        $reactions = [];
+        foreach ($reactionModels as $reactionModel) {
+            try {
+                $reactions[] = $this->fillReactionResponse($reactionModel, $this->db);
+            } catch (RuntimeException $e) {
+                throw new HttpInternalServerErrorException(
+                    request: $request,
+                    message: 'failed to fill reaction',
+                    previous: $e,
+                );
+            }
+        }
+
+        $this->db->commit();
+
+        return $this->jsonResponse($response, $reactions);
     }
 
     public function postReactionHandler(Request $request, Response $response): Response
