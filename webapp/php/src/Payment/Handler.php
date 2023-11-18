d6898e0294bc7f349e0cd37c848a2947da7c5edd
Okada Shohei <okashoicircus0@gmail.com>
2023-11-18 20:49:20 +0900

    [php] implement GET /api/payment

--- webapp/php/src/Payment/Handler.php
+++ webapp/php/src/Payment/Handler.php
@@ -5,14 +5,39 @@ declare(strict_types=1);
 namespace IsuPipe\Payment;
 
 use IsuPipe\AbstractHandler;
+use PDO;
+use PDOException;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Exception\HttpInternalServerErrorException;
 
 class Handler extends AbstractHandler
 {
+    public function __construct(
+        private PDO $db,
+    ) {
+    }
+
     public function getPaymentResult(Request $request, Response $response): Response
     {
-        // TODO: 実装
-        return $response;
+        $this->db->beginTransaction();
+
+        try {
+            $stmt = $this->db->prepare('SELECT IFNULL(SUM(tip), 0) FROM livecomments');
+            $stmt->execute();
+            $totalTip = (int) $stmt->fetchColumn();
+        } catch (PDOException $e) {
+            throw new HttpInternalServerErrorException(
+                request: $request,
+                message: 'failed to count total tip',
+                previous: $e,
+            );
+        }
+
+        $this->db->commit();
+
+        return $this->jsonResponse($response, new PaymentResult(
+            totalTip: $totalTip
+        ));
     }
 }
