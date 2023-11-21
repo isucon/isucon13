4f10d545db53920b0e3b71453bdc1fbdc49b3328
fujiwara <fujiwara.shunichiro@gmail.com>
2023-11-21 10:10:44 +0900

    初期実装はTTL 0

--- webapp/go/user_handler.go
+++ webapp/go/user_handler.go
@@ -257,7 +257,7 @@ func registerHandler(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusInternalServerError, "failed to insert user theme: "+err.Error())
 	}
 
-	if out, err := exec.Command("pdnsutil", "add-record", "u.isucon.dev", req.Name, "A", "30", powerDNSSubdomainAddress).CombinedOutput(); err != nil {
+	if out, err := exec.Command("pdnsutil", "add-record", "u.isucon.dev", req.Name, "A", "0", powerDNSSubdomainAddress).CombinedOutput(); err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, string(out)+": "+err.Error())
 	}
 
