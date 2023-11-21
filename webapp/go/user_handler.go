504ff7a313e6fc5ced14086c7739a74b234d0e38
fujiwara <fujiwara.shunichiro@gmail.com>
2023-11-21 14:39:33 +0900

    fallbackまちがい

--- webapp/go/user_handler.go
+++ webapp/go/user_handler.go
@@ -412,13 +412,13 @@ func fillUserResponse(ctx context.Context, tx *sqlx.Tx, userModel UserModel) (Us
 
 	var image []byte
 	if err := tx.GetContext(ctx, &image, "SELECT image FROM icons WHERE user_id = ?", userModel.ID); err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			image, err = os.ReadFile(fallbackImage)
-			if err != nil {
-				return User{}, err
-			}
+		if !errors.Is(err, sql.ErrNoRows) {
+			return User{}, err
 		}
-		return User{}, err
+		image, err = os.ReadFile(fallbackImage)
+		if err != nil {
+			return User{}, err
+		}
 	}
 	iconHash := sha256.Sum256(image)
 
