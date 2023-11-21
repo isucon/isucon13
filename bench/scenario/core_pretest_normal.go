e60ff5573f61408bebc7790932ae4fb71a2e45c2
fujiwara <fujiwara.shunichiro@gmail.com>
2023-11-21 22:00:22 +0900

    got -> actual

--- bench/scenario/core_pretest_normal.go
+++ bench/scenario/core_pretest_normal.go
@@ -412,7 +412,7 @@ func NormalPostLivecommentPretest(ctx context.Context, testUser *isupipe.User, d
 
 	livestream := livestreams[rand.Intn(len(livestreams))] // ランダムに選ぶ
 	if livestream.Owner.ID != testUser.ID {
-		return fmt.Errorf("自分がownerではないlivestreamが返されました expected:%s got:%s", testUser.Name, livestream.Owner.Name)
+		return fmt.Errorf("自分がownerではないlivestreamが返されました expected:%s actual:%s", testUser.Name, livestream.Owner.Name)
 	}
 
 	if _, err = client.GetLivecommentReports(ctx, livestream.ID); err != nil {
@@ -626,7 +626,7 @@ func NormalModerateLivecommentPretest(ctx context.Context, testUser *isupipe.Use
 	}
 	for _, livestream := range livestreams {
 		if livestream.Owner.ID != testUser.ID {
-			return fmt.Errorf("自分がownerではないlivestreamが返されました expected:%s got:%s", testUser.Name, livestream.Owner.Name)
+			return fmt.Errorf("自分がownerではないlivestreamが返されました expected:%s actual:%s", testUser.Name, livestream.Owner.Name)
 		}
 	}
 	livestream := livestreams[rand.Intn(len(livestreams))] // ランダムに選ぶ
