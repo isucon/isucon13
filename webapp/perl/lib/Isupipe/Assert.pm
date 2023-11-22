78309502101f84b94d2faa96fe54efa76703a0b5
kobaken <kentafly88@gmail.com>
2023-11-23 06:45:08 +0900

    perl: ASSERTを落として、ビルドが通るか念の為確認

--- webapp/perl/lib/Isupipe/Assert.pm
+++ webapp/perl/lib/Isupipe/Assert.pm
@@ -12,7 +12,8 @@ use Carp qw(croak);
 
 # 本番環境ではassertしない
 # 下記のassert_field以外にも、Isupipe::Util#check_paramsでも利用
-use constant ASSERT => ($ENV{PLACK_ENV}||'') ne 'production';
+#use constant ASSERT => ($ENV{PLACK_ENV}||'') ne 'production';
+use constant ASSERT => !!0;
 
 # 開発環境では、型チェックをしてあげる
 sub assert_field($type, $value, $field_name) {
