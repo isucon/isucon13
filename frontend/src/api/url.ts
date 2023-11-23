281177d5007f21bcf2b899b8e64f4ac477ed1fbe
kamijin_fanta <kamijin@live.jp>
2023-11-23 11:27:12 +0900

    frontend: u.isucon.dev -> pipe.u.isucon.dev

--- frontend/src/api/url.ts
+++ frontend/src/api/url.ts
@@ -2,7 +2,7 @@ export function normalizeUrl(
   path: string,
   tenant?: string | undefined,
 ): string {
-  const hostname = tenant ? `${tenant}.u.isucon.dev` : 'u.isucon.dev';
+  const hostname = tenant ? `${tenant}.u.isucon.dev` : 'pipe.u.isucon.dev';
   const port = window.location.port;
   return `https://${hostname}${port ? `:${port}` : ''}${path}`;
 }
